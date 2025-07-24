e819a088584c5a760f13cc99d3155d3ca35db146
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-07-24 18:47:29 +0500

    fixed bug

--- src/features/users/UsersList.jsx
+++ src/features/users/UsersList.jsx
@@ -8,7 +8,7 @@ const UsersList = () => {
 
     const users = useSelector(selectAllUsers)
 
-    const renderedPost = users.map(user => (
+    const renderedUsers = users.map(user => (
         <li className='hover:underline hover:text-white' key={user.id}>
             <Link to={`/user/${user.id}`} >{user.name}</Link>
         </li>
@@ -17,7 +17,7 @@ const UsersList = () => {
   return (
     <section className="bg-gradient-to-b from-gray-800 to-purple-900 px-12 flex flex-col items-center">
       <h2 className='font-bold text-white text-4xl m-3'>Users</h2>
-      <ul className='flex flex-col text-xl gap-6 mb-[21px] text-center'>{renderedPost}</ul>
+      <ul className='flex flex-col text-xl gap-6 mb-[21px] text-center'>{renderedUsers}</ul>
     </section>
   )
 }
